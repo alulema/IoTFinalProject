6aa0c5a954f9a2ddbe5486a7de437378fd09ffef
Alexis <alexis.alulema@gmail.com>
2018-10-10 12:08:24 -0500

    Rolling back 2

--- Client/App/main.c
+++ Client/App/main.c
@@ -11,27 +11,20 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl/curl.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <syslog.h>
 #include <stdbool.h>
-#include <string.h>
-#include <math.h>
 
 #include "tc_error.h"
 #include "tc_state.h"
-#include "tc_math.h"
 
-static const char*  URL             = "http://localhost:5000/api/thermostat";
 static const char*  DAEMON_NAME     = "tcsimd";
 static const char*  TEMP_FILENAME   = "/tmp/temp";
 static const char*  STATE_FILENAME  = "/tmp/status";
 static const char*  WORKING_DIR     = "/";
-static const char*  DEVICE_ID       = "THE-001";
-static const char*  UNIT            = "F";
 
 static const long   SLEEP_DELAY     = 5;
 
@@ -129,51 +122,6 @@ static void _daemonize(void) {
     }
 }
 
-static bool _commit_data(tc_heater_state_t heater_state, float temp) {
-    CURL *curl;
-    CURLcode res;
-
-    char str_temp[6];
-    ftoa(temp, str_temp, 1);
-
-    char json[100];
-    strcpy(json, "{ \"id\": \"");
-    strcat(json, DEVICE_ID);
-    strcat(json, "\", \"status\": \"");
-    strcat(json, heater_state == ON ? "ON" : "OFF");
-    strcat(json, "\", \"measurement\": ");
-    strcat(json, str_temp);
-    strcat(json, ", \"unit\": \"");
-    strcat(json, UNIT);
-    strcat(json, "\" }");
-
-    curl = curl_easy_init();
-
-    if (curl) {
-        struct curl_slist *hs=NULL;
-        hs = curl_slist_append(hs, "Content-Type: application/json");
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hs);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(json));
-        res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-            return false;
-        }
-
-        curl_easy_cleanup(curl);
-    } else {
-        return false;
-    }
-
-    return true;
-}
-
 /**
  * This runs the simulation. We essentially have a loop which
  * reads the heater state, adjust the temperature based on this
@@ -202,7 +150,6 @@ static void _run_simulation(void) {
         err = tc_write_temperature(TEMP_FILENAME, temp);
         if (err != OK) _exit_process(err);
 
-        _commit_data(heater_state, temp);
         // Take a bit of a nap.
         sleep(SLEEP_DELAY);
     }
@@ -254,7 +201,7 @@ static void _configure(void) {
  */
 int main(void) {
     // Daemonize the process.
-    // _daemonize();
+    _daemonize();
 
     // Set up appropriate files if they don't exist.
     _configure();
